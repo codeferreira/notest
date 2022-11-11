663ab3dcd41c49f8de93301f6857c906f2c9351a
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-11 18:28:31 -0300

    refactor: routing system

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:notest/firebase_options.dart';
 import 'package:notest/views/login_view.dart';
+import 'package:notest/views/register_view.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -10,6 +13,32 @@ void main() async {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const LoginView(),
+    routes: {
+      '/': (context) => const HomePage(),
+      '/login': (context) => const LoginView(),
+      '/register': (context) => const RegisterView(),
+    },
+    initialRoute: '/',
   ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            return const LoginView();
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
 }
