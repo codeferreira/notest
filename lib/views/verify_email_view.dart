9d0d7ec722bf442d6094d3bb878fd7aa6eab957c
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-11 20:14:16 -0300

    chore: add reset button to start register process again

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:notest/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -17,7 +18,12 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       ),
       body: Column(
         children: [
-          const Text('Please verify your email address'),
+          const Text(
+            "We've sent you an email verification. Please open it to verify yout account.",
+          ),
+          const Text(
+            "If you haven't received the email, press the button bellow",
+          ),
           TextButton(
             onPressed: () async {
               final user = FirebaseAuth.instance.currentUser;
@@ -26,6 +32,19 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
             },
             child: const Text('Send email verification'),
           ),
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+
+              if (!mounted) return;
+
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                (route) => false,
+              );
+            },
+            child: const Text('Restart'),
+          )
         ],
       ),
     );
