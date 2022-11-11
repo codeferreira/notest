6c448a62d738c53f90a234a9f1d3ace17773028b
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-11 18:39:58 -0300

    feat: add verify email route

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:notest/firebase_options.dart';
 import 'package:notest/views/login_view.dart';
 import 'package:notest/views/register_view.dart';
+import 'package:notest/views/verify_email_view.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -17,6 +19,7 @@ void main() async {
       '/': (context) => const HomePage(),
       '/login': (context) => const LoginView(),
       '/register': (context) => const RegisterView(),
+      '/verify-email': (context) => const VerifyEmailView(),
     },
     initialRoute: '/',
   ));
@@ -34,7 +37,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+
+            if (user != null) {
+              if (user.emailVerified) {
+                return const Text('Email verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
           default:
             return const CircularProgressIndicator();
         }
