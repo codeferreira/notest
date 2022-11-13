98dd85e79f20e1f8b3681b64c246a4cebb3e4be5
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-13 03:10:16 -0300

    chore: add initialize to abstract auth provider class

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -3,6 +3,8 @@ import 'package:notest/services/auth/auth_user.dart';
 abstract class AuthProvider {
   AuthUser? get currentUser;
 
+  Future<void> initialize();
+
   Future<AuthUser> logIn({
     required String email,
     required String password,
