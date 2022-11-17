aa38b71903db23354bac77afe62eab3dd0502408
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-17 02:15:57 -0300

    fix: connection state cases

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -78,10 +78,8 @@ class _NotesViewState extends State<NotesView> {
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
+                    case ConnectionState.active:
                       return const Text('Waiting for notes...');
-
-                    case ConnectionState.done:
-                      return const Text('Notes');
 
                     default:
                       return const CircularProgressIndicator();
