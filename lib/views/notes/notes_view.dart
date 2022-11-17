4430999da764f9fa280717b1cf9a85e7c04ce2a9
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-17 03:35:51 -0300

    refactor: remove logout dialog and move notes list into separated view

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -3,6 +3,8 @@ import 'package:notest/constants/routes.dart';
 import 'package:notest/enums/menu_action.dart';
 import 'package:notest/services/auth/auth_service.dart';
 import 'package:notest/services/crud/notes_service.dart';
+import 'package:notest/utilities/dialogs/logout_dialog.dart';
+import 'package:notest/views/notes/notes_list_view.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -75,20 +77,11 @@ class _NotesViewState extends State<NotesView> {
                     case ConnectionState.active:
                       if (snapshot.hasData) {
                         final allNotes = snapshot.data as List<DatabaseNote>;
-                        return ListView.builder(
-                          itemCount: allNotes.length,
-                          itemBuilder: (context, index) {
-                            final note = allNotes[index];
-                            print(allNotes);
-                            return ListTile(
-                              title: Text(
-                                note.text,
-                                maxLines: 1,
-                                softWrap: true,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            );
+                        return NotesListView(
+                          onDeleteNote: (note) async {
+                            await _notesService.deleteNote(id: note.id);
                           },
+                          notes: allNotes,
                         );
                       }
 
@@ -105,30 +98,4 @@ class _NotesViewState extends State<NotesView> {
       ),
     );
   }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Log Out'),
-        content: const Text('Are you sure you want to sign out?'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(false);
-            },
-            child: const Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-            },
-            child: const Text('Confirm'),
-          ),
-        ],
-      );
-    },
-  ).then((value) => value ?? false);
 }
