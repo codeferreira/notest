5292497816360c5aa1ebc14401acfc97c3175576
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-17 05:50:51 -0300

    feat: add id as default parameter to AuthUser

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -1,18 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:firebase_auth/firebase_auth.dart' show User;
 import 'package:flutter/foundation.dart';
 
 @immutable
 class AuthUser {
   final bool isEmailVerified;
-  final String? email;
+  final String email;
+  final String id;
 
   const AuthUser({
     required this.isEmailVerified,
     required this.email,
+    required this.id,
   });
 
   factory AuthUser.fromFirebase(User user) => AuthUser(
-        email: user.email,
+        id: user.uid,
+        email: user.email!,
         isEmailVerified: user.emailVerified,
       );
 }
