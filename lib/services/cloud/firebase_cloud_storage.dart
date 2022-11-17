ec99b441d0a346fab3da63f8ba84f6781200a88a
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-17 05:52:15 -0300

    feat: implement cloud firestore service to handle notes

---
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:notest/services/cloud/cloud_note.dart';
+import 'package:notest/services/cloud/cloud_storage_constants.dart';
+import 'package:notest/services/cloud/could_storage_exceptions.dart';
+
+class FirebaseCloudStorage {
+  static final FirebaseCloudStorage _shared =
+      FirebaseCloudStorage._sharedInstance();
+  FirebaseCloudStorage._sharedInstance();
+  factory FirebaseCloudStorage() => _shared;
+
+  final notes = FirebaseFirestore.instance.collection('notes');
+
+  Stream<Iterable<CloudNote>> allnotes({required String ownerUserId}) =>
+      notes.snapshots().map((event) => event.docs
+          .map((doc) => CloudNote.fromSnapshot(doc))
+          .where((note) => note.ownerUserId == ownerUserId));
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    try {
+      return await notes
+          .where(
+            ownerUserIdFieldName,
+            isEqualTo: ownerUserId,
+          )
+          .get()
+          .then(
+            (value) => value.docs.map(
+              (doc) {
+                return CloudNote(
+                  documentId: doc.id,
+                  ownerUserId: doc.data()[ownerUserIdFieldName] as String,
+                  text: doc.data()[textFieldName] as String,
+                );
+              },
+            ),
+          );
+    } catch (_) {
+      throw CouldNotGetAllNotesException();
+    }
+  }
+
+  Future<void> updateNote({
+    required String documentId,
+    required String text,
+  }) async {
+    try {
+      await notes.doc(documentId).update({textFieldName: text});
+    } catch (_) {
+      throw CouldNotUpdateNoteException();
+    }
+  }
+
+  Future<void> deleteNote({required String documentId}) async {
+    try {
+      await notes.doc(documentId).delete();
+    } catch (_) {
+      throw CouldNotDeleteNoteException();
+    }
+  }
+
+  void createNewNote({required String ownerUserId}) async {
+    await notes.add({
+      ownerUserIdFieldName: ownerUserId,
+      textFieldName: '',
+    });
+  }
+}
