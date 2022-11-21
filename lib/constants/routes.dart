1ca42a60ba9c97e3ded6fca53f54810ea722c51c
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-11-21 19:57:13 -0300

    refactor: move routes to auth bloc

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,5 +1,1 @@
-const loginRoute = '/login';
-const registerRoute = '/register';
-const verifyEmailRoute = '/verify-email';
-const notesRoute = '/notes';
 const createOrUpdateNoteRoute = '/notes/create-update';
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notest/constants/routes.dart';
-import 'package:notest/views/login_view.dart';
 import 'package:notest/views/notes/create_update_note_view.dart';
-import 'package:notest/views/notes/notes_view.dart';
-import 'package:notest/views/register_view.dart';
-import 'package:notest/views/verify_email_view.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  loginRoute: (context) => const LoginView(),
-  registerRoute: (context) => const RegisterView(),
-  verifyEmailRoute: (context) => const VerifyEmailView(),
-  notesRoute: (context) => const NotesView(),
   createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
 };
